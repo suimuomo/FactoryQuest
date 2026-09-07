8342f4623fb847f0e610d4f3c85320d173e0c983
suimuomo <134160083+suimuomo@users.noreply.github.com>
2026-09-07 13:12:14 +0800

    feat: add player model and database context

---
+++ FactoryQuest.Server/Data/GameDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FactoryQuest.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FactoryQuest.Server.Data
+{
+    public class GameDbContext : DbContext
+    {
+        public GameDbContext(DbContextOptions<GameDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
---
+++ FactoryQuest.Server/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace FactoryQuest.Server.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Level { get; set; }
+
+        public int Experience { get; set; }
+
+        public int Gold { get; set; }
+    }
+}
