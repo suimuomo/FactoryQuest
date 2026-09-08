15ebd39e9708d36bae43c4eb173ac42d6b7885a9
suimuomo <134160083+suimuomo@users.noreply.github.com>
2026-09-08 11:38:48 +0800

    connect player api to database

--- FactoryQuest.Server/Controllers/PlayerController.cs
+++ FactoryQuest.Server/Controllers/PlayerController.cs
@@ -1,23 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
+using FactoryQuest.Server.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace FactoryQuest.Server.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class PlayerController : ControllerBase
+namespace FactoryQuest.Server.Controllers
 {
-    [HttpGet]
-    public IActionResult GetPlayer()
-    {
-        var player = new
-        {
-            Id = 1,
-            Name = "新人探索者",
-            Level = 1,
-            Experience = 0,
-            Gold = 100
-        };
 
-        return Ok(player);
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        private readonly GameDbContext _db;
+
+        public PlayerController(GameDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetPlayer()
+        {
+            var player = _db.Players.FirstOrDefault();
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(player);
+        }
+
+
+        [HttpPost("{id}/explore")]
+        public async Task<IActionResult> Explore(int id)
+        {
+            var player = await _db.Players.FindAsync(id);
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            player.Experience += 10;
+            player.Gold += 20;
+
+            await _db.SaveChangesAsync();
+
+            return Ok(player);
+        }
     }
 }
--- FactoryQuest.Server/Program.cs
+++ FactoryQuest.Server/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using FactoryQuest.Server.Data;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<GameDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 var app = builder.Build();
 
